fa06f266401487dacdccee9da142d3241b29df2c
joao.luzio <joao.luzio@gmail.com>
2020-06-15 14:14:20 +0100

    Initial import

---
+++ playground/src/test/java/com/example/ms/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
